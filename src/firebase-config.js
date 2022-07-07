32a9f344595539eebb5abff90e5525725844dcb5
schau1121 <simon.chau1121@gmail.com>
2022-07-07 12:55:40 -0700

    Initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdRl7fL80OqX9blQO_sa-DUqRwCnmc2TY",
+    authDomain: "pdf-searcher-14273.firebaseapp.com",
+    projectId: "pdf-searcher-14273",
+    storageBucket: "pdf-searcher-14273.appspot.com",
+    messagingSenderId: "212009016279",
+    appId: "1:212009016279:web:10b0bce1b19d1a094fc992"
+};
+
+const app = initializeApp(firebaseConfig);
